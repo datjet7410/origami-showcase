7dbdb6447579a62af05ec5682daf3b937dbdbbcb
datjet7410 <dat7410dat@gmail.com>
2021-12-08 13:56:12 +0700

    update netlìy function

---
+++ src/error.md
@@ -0,0 +1,9 @@
+---
+title: Đã có lỗi xảy ra 🥲🥲🥲
+layout: layouts/base.jsx
+---
+
+Chúng tôi rất tiếc, vui lòng
+<a href="mailto:dat.nguyendatjet7410@hcmut.edu.vn">
+Gửi mail đến tác giả
+</a>
--- src/netlify/functions/register.js
+++ src/netlify/functions/register.js
@@ -33,6 +33,7 @@ function parseMultipartForm(event) {
     busboy.write(event.body);
   });
 }
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -41,15 +42,21 @@ exports.handler = async function (event, context) {
   switch (event.httpMethod) {
     case "POST": {
       const { image, ...fields } = await parseMultipartForm(event);
-
-      const { data, error } = await supabase
-        .from("origami-register")
-        .insert([fields]);
-      const { data, error } = await supabase.storage
-        .from("origami-register")
-        .upload(image.filename, image.content);
-
       const redirectUrl = "/thanks";
+
+      await supabase
+        .from("origami-register")
+        .insert([fields])
+        .catch((err) => {
+          redirectUrl = "/error";
+        });
+      await supabase.storage
+        .from("origami-register")
+        .upload(image.filename, image.content)
+        .catch((err) => {
+          redirectUrl = "/error";
+        });
+
       return {
         statusCode: 302,
         headers: {
